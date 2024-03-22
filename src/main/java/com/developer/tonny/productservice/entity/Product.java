d556970f3706a83d05e3fab3f68b071cf9f00766
Tonny Oseko <tonnyoseko0@gmail.com>
2024-03-22 16:05:21 +0300

    create product entity for product service

---
+++ src/main/java/com/developer/tonny/productservice/entity/Product.java
@@ -0,0 +1,25 @@
+package com.developer.tonny.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_products", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "product_code", name = "UNIQ_PRODUCT_CODE")
+})
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String product_code;
+}
